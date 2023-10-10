9301b691c673e031ab7d174a30646009379f3414
BPGTT <grupobpgtt@gmail.com>
2023-10-09 21:28:21 -0300

    Aprendendo NODEJS

--- 08express/app.js
+++ 08express/app.js
@@ -4,4 +4,11 @@ const express = require('express');
 //Criar uma variavel para ter acesso as funcionalidades do express
 const app = express();
 
+app.get('/', function(req, res){
+    res.write('Utilizando o Express com nodemon!')
+    res.end();
+});
 
+app.listen(3000, ()=>{
+    console.log('Servidor rodando na porta 3000');
+});
